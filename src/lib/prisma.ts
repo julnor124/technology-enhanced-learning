4b507af30f581dadf92bfed2e62ea1e78921b592
Julia Nordqvist <juliaanordqvist@hotmail.com>
2025-11-20 15:41:10 +0100

    prisma fix deploy1

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { PrismaClient } = require("@prisma/client");
+import { PrismaClient } from "@prisma/client";
 
 type PrismaClientType = InstanceType<typeof PrismaClient>;
 
